40b21f82dd2719c240724d55d43e408898977d9f
modi-poojan <80687912+modi-poojan@users.noreply.github.com>
2021-11-23 23:48:16 +0530

    UseCase2: Max num in Float with test cases added

---
+++ src/com/genric/Test_Maximum.java
@@ -0,0 +1,21 @@
+package com.genric;
+
+public class Test_Maximum{
+	
+	public static void main(String[] args) {
+		
+		System.out.println(" Welcome to Java Generics Maximum number of 3 ");
+		Find_Max_Genrics call = new Find_Max_Genrics();
+		call.checkIntegers(8989, 7989 , 5985);			// test case 1
+		call.checkIntegers(79, 80, 59);					// test case 2				
+		call.checkIntegers(50, 77, 89);					// test case 3
+		
+		System.out.println("\n\n\n");
+		
+		call.checkFloat(13.f, 6.f, 12.f);				// test case 1
+		call.checkFloat(777.1f, 889.99f, 33.7f);		// test case 2				
+		call.checkFloat(77.f, 445.f, 779.f);			// test case 3
+		
+		
+	}
+} 
