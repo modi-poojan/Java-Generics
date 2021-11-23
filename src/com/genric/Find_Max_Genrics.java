ebf3678a24206f779cabfbaa6eba4c4f28539d57
modi-poojan <80687912+modi-poojan@users.noreply.github.com>
2021-11-23 23:50:38 +0530

    UseCase3: Max of 3 String using generic added

--- src/com/genric/Find_Max_Genrics.java
+++ src/com/genric/Find_Max_Genrics.java
@@ -29,6 +29,15 @@ public class Find_Max_Genrics {
 		
 	}
 	
-	
+	public void checkString(String first, String second, String third) {
+		if(first.compareTo(second) > 0 && first.compareTo(third) > 0) {
+			System.out.println(" First is largest ");
+		}
+		else if(second.compareTo(first) > 0 && second.compareTo(third) > 0) {
+			System.out.println(" Second is largest");
+		}
+		else
+			System.out.println(" Third is largest");
+	}
 
 }
